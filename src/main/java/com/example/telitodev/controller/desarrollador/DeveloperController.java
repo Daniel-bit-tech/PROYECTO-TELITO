59f00f428d9ea15abf97114a19b977431d76889f
Luis <a20221203@pucp.edu.pe>
2025-09-10 20:48:42 -0500

    Crud catalogo apis y filtrado basico
    Se muestra datos de bd y se filtra por dominio, tag y nombre

--- src/main/java/com/example/telitodev/controller/desarrollador/DeveloperController.java
+++ src/main/java/com/example/telitodev/controller/desarrollador/DeveloperController.java
@@ -1,15 +1,42 @@
 package com.example.telitodev.controller.desarrollador;
 
+import com.example.telitodev.repository.ApiRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 @Controller
+@RequestMapping("/dev")
+@PreAuthorize("hasRole('DEV')")
 public class DeveloperController {
 
-    @GetMapping("/developer")
+
+    @GetMapping("/home")
     public String showDeveloperView() {
+        System.out.println("entro");
+
+
 
 
         return "desarrollador/developer";
+    }
+
+    @GetMapping("/catalogo")
+    public String developerDashboard(Model model, Authentication authentication) {
+        model.addAttribute("smg", authentication.getName());
+
+
+
+
+
+        return "desarrollador/apis";
     }
 }
---
+++ src/main/java/com/example/telitodev/repository/ApiRepository.java
@@ -0,0 +1,26 @@
+package com.example.telitodev.repository;
+
+import com.example.telitodev.entity.Api;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ApiRepository extends JpaRepository<Api, Integer> {
+
+    List<Api> findByDominio(String dominio);
+
+    List<Api> findByTag(String tag);
+
+    @Query(value = "SELECT a.* FROM api a " +
+                    "WHERE (:search IS NULL OR LOWER(a.nombre) LIKE LOWER(CONCAT('%', :search, '%')))" +
+                    "AND (:dominios IS NULL OR FIND_IN_SET(a.dominio, :dominios)) " +
+                    "AND (:tags IS NULL OR FIND_IN_SET(a.tag, :tags))", nativeQuery = true)
+    List<Api> findByFilters(@Param("search") String search,
+                            @Param("dominios") String dominios,
+                            @Param("tags") String tags);
+
+}
