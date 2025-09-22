4f7948773d0088a163095c286a7abaa5be9c5e63
Luis <a20221203@pucp.edu.pe>
2025-09-21 21:39:53 -0500

    feat: crud lista de Proyectos

--- src/main/java/com/example/telitodev/entity/Proyecto.java
+++ src/main/java/com/example/telitodev/entity/Proyecto.java
@@ -37,9 +37,91 @@ public class Proyecto {
     private String repositorio;
 
     @Column(name = "publico")
-    private Boolean Publico;
+    private Boolean publico;
 
-    @OneToMany(mappedBy = "proyecto", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Column(name = "activo")
+    private Boolean activo;
+
+    //Relaciones
+    @OneToMany(mappedBy = "proyecto", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<ProyectoHasApi> proyectoHasApis;
 
+    //Get y Set
+
+    public Integer getIdProyecto() {
+        return idProyecto;
+    }
+    public void setIdProyecto(Integer idProyecto) {
+        this.idProyecto = idProyecto;
+    }
+
+    public Organizacion getOrganizacion() {
+        return organizacion;
+    }
+    public void setOrganizacion(Organizacion organizacion) {
+        this.organizacion = organizacion;
+    }
+
+    public Usuario getUsuarioLider() {
+        return usuarioLider;
+    }
+    public void setUsuarioLider(Usuario usuarioLider) {
+        this.usuarioLider = usuarioLider;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public String getRepositorio() {
+        return repositorio;
+    }
+    public void setRepositorio(String repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public Boolean getPublico() {
+        return publico;
+    }
+    public void setPublico(Boolean publico) {
+        this.publico = publico;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public List<ProyectoHasApi> getProyectoHasApis() {
+        return proyectoHasApis;
+    }
+    public void setProyectoHasApis(List<ProyectoHasApi> proyectoHasApis) {
+        this.proyectoHasApis = proyectoHasApis;
+    }
 }
--- src/main/java/com/example/telitodev/entity/ProyectoHasApi.java
+++ src/main/java/com/example/telitodev/entity/ProyectoHasApi.java
@@ -12,7 +12,7 @@ public class ProyectoHasApi {
     private ProyectoHasApiId proyectoHasApiId;
 
     @MapsId("idProyecto")
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "idProyecto")
     private Proyecto proyecto;
 
@@ -27,12 +27,63 @@ public class ProyectoHasApi {
     @Column(name = "proposito")
     private String proposito;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "idVersion")
     private VersionApi version;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "idEntorno")
     private Entorno entorno;
 
+
+    //Get y Set
+
+    public ProyectoHasApiId getProyectoHasApiId() {
+        return proyectoHasApiId;
+    }
+    public void setProyectoHasApiId(ProyectoHasApiId proyectoHasApiId) {
+        this.proyectoHasApiId = proyectoHasApiId;
+    }
+
+    public Proyecto getProyecto() {
+        return proyecto;
+    }
+    public void setProyecto(Proyecto proyecto) {
+        this.proyecto = proyecto;
+    }
+
+    public Api getApi() {
+        return api;
+    }
+    public void setApi(Api api) {
+        this.api = api;
+    }
+
+    public Date getFechaAsociacion() {
+        return fechaAsociacion;
+    }
+    public void setFechaAsociacion(Date fechaAsociacion) {
+        this.fechaAsociacion = fechaAsociacion;
+    }
+
+    public String getProposito() {
+        return proposito;
+    }
+    public void setProposito(String proposito) {
+        this.proposito = proposito;
+    }
+
+    public VersionApi getVersion() {
+        return version;
+    }
+    public void setVersion(VersionApi version) {
+        this.version = version;
+    }
+
+    public Entorno getEntorno() {
+        return entorno;
+    }
+    public void setEntorno(Entorno entorno) {
+        this.entorno = entorno;
+    }
 }
