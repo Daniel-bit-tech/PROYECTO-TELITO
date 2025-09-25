c8cca0109fd3b4af11aa20c7bfd4ec9214c4a184
jasnai10 <a20223806@pucp.edu.pe>
2025-09-25 18:00:24 -0500

    feat: Actualización de qa

--- src/main/java/com/example/telitodev/entity/Reporte.java
+++ src/main/java/com/example/telitodev/entity/Reporte.java
@@ -7,72 +7,81 @@ import java.util.List;
 @Entity
 @Table(name = "reporte")
 public class Reporte {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idReporte")
     private Integer idReporte;
-    
+
     @Column(name = "descripcion", length = 200)
     private String descripcion;
-    
+
     @Column(name = "fecha_creacion", nullable = false)
     private Timestamp fechaCreacion;
-    
+
+    @Column(name = "estado", length = 50)
+    private  String estado;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "API_idAPI", nullable = false)
     private Api api;
-    
+
     @OneToMany(mappedBy = "reporte", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Issue> issues;
-    
+
     // Constructores
     public Reporte() {}
-    
-    public Reporte(String descripcion, Timestamp fechaCreacion, Api api) {
+
+    public Reporte(String descripcion, String estado,Timestamp fechaCreacion, Api api) {
         this.descripcion = descripcion;
+        this.estado = estado;
         this.fechaCreacion = fechaCreacion;
         this.api = api;
     }
-    
+
     // Getters y Setters
     public Integer getIdReporte() {
         return idReporte;
     }
-    
+
     public void setIdReporte(Integer idReporte) {
         this.idReporte = idReporte;
     }
-    
+
     public String getDescripcion() {
         return descripcion;
     }
-    
+
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-    
+
+    public String getEstado() { return estado;}
+
+    public void setEstado(String estado) { this.estado = estado;}
+
     public Timestamp getFechaCreacion() {
         return fechaCreacion;
     }
-    
+
     public void setFechaCreacion(Timestamp fechaCreacion) {
         this.fechaCreacion = fechaCreacion;
     }
-    
+
     public Api getApi() {
         return api;
     }
-    
+
     public void setApi(Api api) {
         this.api = api;
     }
-    
+
     public List<Issue> getIssues() {
         return issues;
     }
-    
+
     public void setIssues(List<Issue> issues) {
         this.issues = issues;
     }
+
 }
