d8d9a0fe264f2b5c0536db8e9ed276aa67a467b1
david <a20224848@gmail.com>
2025-10-02 11:12:18 -0500

    Cambios vista/Catalogo

--- src/main/java/com/example/telitodev/repository/TagRepository.java
+++ src/main/java/com/example/telitodev/repository/TagRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Integer> {
+    //esta tambien funciona tanto para DEV-PO
 }
