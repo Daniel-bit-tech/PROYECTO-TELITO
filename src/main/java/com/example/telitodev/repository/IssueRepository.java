b751f96505090552dc0f551a1595391d25450114
jasnai10 <a20223806@pucp.edu.pe>
2025-10-11 17:57:02 -0500

    merge con rama Luis y JairParc

--- src/main/java/com/example/telitodev/repository/IssueRepository.java
+++ src/main/java/com/example/telitodev/repository/IssueRepository.java
@@ -3,9 +3,27 @@ package com.example.telitodev.repository;
 import com.example.telitodev.entity.Issue;
 import com.example.telitodev.entity.IssueId;
 import com.example.telitodev.entity.Reporte;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 @Repository
 public interface IssueRepository extends JpaRepository<Issue, IssueId> {
+    @Query("SELECT i FROM Issue i " +
+            "WHERE (:estados IS NULL OR i.estado IN :estados) " +
+            "AND (:inicio IS NULL OR i.fechaCreacion >= :inicio) " +
+            "AND (:fin IS NULL OR i.fechaCreacion <= :fin) " +
+            "AND (:nombre IS NULL OR LOWER(i.descripcion) LIKE LOWER(CONCAT('%', :nombre, '%')))")
+    Page<Issue> findByFilters(@Param("estados") List<String> estados,
+                              @Param("inicio") Timestamp inicio,
+                              @Param("fin") Timestamp fin,
+                              @Param("nombre") String nombre,
+                              Pageable pageable);
+
 }
